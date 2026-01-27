fd64a56c03083229880cf74a52f83ee423f0cbc7
BallOfEnergy01 <66693744+BallOfEnergy1@users.noreply.github.com>
2026-01-26 20:18:39 -0600

    Cleanup

--- src/main/java/com/gamma/gammalib/asm/util/IteratorDescriptor.java
+++
@@ -1,9 +0,0 @@
-package com.gamma.gammalib.asm.util;
-
-import org.spongepowered.asm.lib.tree.AbstractInsnNode;
-
-import com.github.bsideup.jabel.Desugar;
-
-@Desugar
-public record IteratorDescriptor(StackRebuilder rebuilder, MethodInformation methodInfo, AbstractInsnNode start,
-    AbstractInsnNode comparisonNode, AbstractInsnNode end) {}
